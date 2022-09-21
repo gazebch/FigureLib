08b171e3bc364cd15e51e55be5eafb3a5c0f1e3d
Gazeba <sipion46@mail.ru>
2022-09-21 17:51:04 +0300

    Добавьте файлы проекта.

---
+++ Area.cs
@@ -0,0 +1,28 @@
+﻿namespace FigureLib
+{
+    public class Area
+    {
+        public static double AreaTriangle(double a, double b, double c)  //площадь треугольника
+        {
+            var p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public static double AreaCircle(double r) //площадь круга
+        {            
+            return Math.PI * Math.Pow(r, 2);
+        }
+        
+        public static void SquarenessCheck(double a, double b, double c) //проверка треугольника на прямоульность
+        {
+            if (c * c == a * a + b * b)
+            {
+                Console.WriteLine("Треугольник прямоугольный");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник непрямоугольный");
+            }
+        }
+    }
+}
